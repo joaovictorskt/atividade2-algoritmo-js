ed3f3f490b8d2c6dfd6ea027ee59b64aa62895f7
João Victor <89541042+joaovictorskt@users.noreply.github.com>
2021-09-14 12:00:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ if (datadoevento >21) {
     console.log ("Cadastro Permitido")
 } else console.log ("Cadastro Negado"); ("Data Invalida")
 
+//CADASTRO CONCLUÍDO
+//INSERIR IDADE
 
 var idade =("18")
 
 if (idade >=18) {
 console.log ("Idade Permitida")
 } else console.log ("Idade não Permitida"); ("Cadastro Negado")
+
+//IDADE PERMITIDA
+//CONFERIR LISTA DE PARTICIPANTES 
 
 var participantes = "85"
 
@@ -20,4 +25,6 @@ var participantes = "85"
 if (participantes <100){
     console.log ("Cadastro Concluído")
 } else console.log ("Cadastro Negado"); ("Limite de Participantes atingido")
+
+//CADASTRO CONCLUÍDO (AGUARDAR DIA DO EVENTO)
 
