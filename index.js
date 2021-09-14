7f8b4b25f92ed25074ac0c488a93ed6725f66cd1
joaovictorskt <joaobernardoskt@outlook.com>
2021-09-14 10:30:52 -0300

    atividade2-algoritmo-js

---
+++ index.js
@@ -0,0 +1,23 @@
+var datadoevento ="23"
+
+//data atual "21"
+
+if (datadoevento >21) {
+    console.log ("Cadastro Permitido"); console.log ("Insira seu nome")
+} else console.log ("Cadastro Negado"); ("Data Invalida")
+
+
+var idade =("18")
+
+if (idade >=18) {
+console.log ("Idade Permitida"); console.log ("Insira uma senha")
+} else console.log ("Idade não Permitida"); ("Cadastro Negado")
+
+var participantes = "85"
+
+//limite de participantes "100"
+
+if (participantes <100){
+    console.log ("Cadastro Permitido"); console.log ("Aguarde o dia do Evento")
+} else console.log ("Cadastro Negado"); ("Limite de Participantes atingido")
+
