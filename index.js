4de89097bfac0b4608b94ee8ecc45fd59af3afa2
João Victor <89541042+joaovictorskt@users.noreply.github.com>
2021-09-24 17:48:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
-var datadoevento ="23"
+var datadoevento =23; var datadoevento =(window.prompt("Insira a data do evento: "));
+ 
+if (datadoevento >21){
+    console.log ("Cadastro Permitido"); 
+} else 
+    window.alert("Data invalida"); 
+    
+    while (datadoevento <21){
+        datadoevento = window.prompt("Data do evento invalida, insira outra data");
+    }
 
-//data atual "21"
-
-if (datadoevento >21) {
-    console.log ("Cadastro Permitido")
-} else console.log ("Cadastro Negado"); ("Data Invalida")
-
-//CADASTRO CONCLUÍDO
-//INSERIR IDADE
-
-var idade =("18")
+// CADASTRO PERMITIDO
+// INSERIR IDADE
+ 
+var idade =("18"); var idade =(window.prompt("Insira idade: ")) 
 
 if (idade >=18) {
-console.log ("Idade Permitida")
-} else console.log ("Idade não Permitida"); ("Cadastro Negado")
+console.log ("Idade Permitida") 
+} else console.log ("Idade não Permitida"); 
+ 
+while (idade <"18"){
+    idade = window.prompt("idade invalida, insira outra idade");
+}
 
-//IDADE PERMITIDA
-//CONFERIR LISTA DE PARTICIPANTES 
+// IDADE PERMITIDA
+// CONFERIR LISTA DE PARTICIPANTES
 
-var participantes = "85"
+var participantes = 85; window.alert("Conferindo lista de participantes") 
 
 //limite de participantes "100"
 
 if (participantes <100){
-    console.log ("Cadastro Concluído")
-} else console.log ("Cadastro Negado"); ("Limite de Participantes atingido")
+    console.log ("Cadastro Concluído"); console.log ("Aguarde o dia do evento"); var datadoevento =window.alert("Cadastro concluído! Aguarde o dia do evento")
+} else console.log ("Cadastro Negado"); 
 
 //CADASTRO CONCLUÍDO (AGUARDAR DIA DO EVENTO)
 
